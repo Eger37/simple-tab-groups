a10b2aed41127c0147fe6816c4c5a44f51ba3273
Danyil <danyil@logisoft.ai>
2026-06-29 02:46:04 +0300

    fix(options): guard gist-name validation against undefined

--- addon/src/options/github-gist-fields.vue
+++ addon/src/options/github-gist-fields.vue
@@ -42,7 +42,7 @@ export default {
         },
         internalGistName: {
             get() {
-                return this.gistName;
+                return this.gistName || '';
             },
             set(value) {
                 this.$emit('update:gistName', value);
